41066b450b279b09aa9e8365abf9934ba2ca6ee3
TienNN <nhuttien11427@gmail.com>
2026-06-29 15:52:11 +0700

    Add files via upload

---
+++ js/gallery.js
@@ -0,0 +1,10 @@
+/*Name this external file gallery.js*/
+function upDate(previewPic){
+	console.log(previewPic.alt, previewPic.src);
+	document.getElementById('image').style.backgroundImage = "url('" + previewPic.src + "')";
+	document.getElementById('image').textContent = previewPic.alt;
+}
+function unDo(){
+	document.getElementById('image').style.backgroundImage = "url('')";
+	document.getElementById('image').textContent = "Hover over an image below to display here.";
+}
