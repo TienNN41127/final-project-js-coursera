4e84d2c885c40e64094a4d8db371402995850175
TienNN <nhuttien11427@gmail.com>
2026-06-29 16:03:32 +0700

    Update gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -3,7 +3,18 @@ function upDate(previewPic){
 	document.getElementById('image').style.backgroundImage = "url('" + previewPic.src + "')";
 	document.getElementById('image').textContent = previewPic.alt;
 }
+
 function unDo(){
 	document.getElementById('image').style.backgroundImage = "url('')";
 	document.getElementById('image').textContent = "Hover over an image below to display here.";
+}
+
+function setupGallery(){
+	console.log("setupGallery triggered - page loaded");
+
+	var previewImages = document.getElementsByClassName('preview');
+
+	for(var i = 0; i < previewImages.length; i++){
+		previewImages[i].setAttribute('tabindex', '0');
+	}
 }
